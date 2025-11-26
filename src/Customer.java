3c39a5bdadf553fc695c6b619f55cf9fbb87de3e
unknown <jjsoares@minha.fag.edu.br>
2025-11-25 22:25:56 -0300

    Commit 3

--- src/Customer.java
+++ src/Customer.java
@@ -3,23 +3,31 @@ import java.util.List;
 
 public class Customer {
 
+
     private String name;
     private List<Rental> rentals = new ArrayList<>();
+
 
     public Customer(String name) {
         this.name = name;
     }
 
+
     public void addRental(Rental rental) {
         rentals.add(rental);
     }
+
 
     public String getName() {
         return name;
     }
 
+
+    // MÉTODO amountFor EXTRAÍDO (commit 3)
     private double amountFor(Rental each) {
         double thisAmount = 0;
+
+
         switch (each.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
@@ -27,9 +35,11 @@ public class Customer {
                     thisAmount += (each.getDaysRented() - 2) * 1.5;
                 break;
 
+
             case Movie.NEW_RELEASE:
                 thisAmount += each.getDaysRented() * 3;
                 break;
+
 
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
@@ -40,24 +50,30 @@ public class Customer {
         return thisAmount;
     }
 
+
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
 
+
         for (Rental each : rentals) {
             double thisAmount = amountFor(each);
             frequentRenterPoints++;
 
+
             if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1)
                 frequentRenterPoints++;
+
 
             result.append("\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n");
             totalAmount += thisAmount;
         }
 
+
         result.append("Amount owed is " + totalAmount + "\n");
         result.append("You earned " + frequentRenterPoints + " frequent renter points");
+
 
         return result.toString();
     }
--- src/Rental.java
+++ src/Rental.java
@@ -1,18 +1,22 @@
 public class Rental {
 
-    private Movie _movie;
-    private int _daysRented;
 
-    public Rental(Movie movie, int daysRented) {
-        _movie = movie;
-        _daysRented = daysRented;
-    }
+private Movie movie;
+private int daysRented;
 
-    public int getDaysRented() {
-        return _daysRented;
-    }
 
-    public Movie getMovie() {
-        return _movie;
-    }
+public Rental(Movie movie, int daysRented) {
+    this.movie = movie;
+    this.daysRented = daysRented;
+}
+
+
+public int getDaysRented() {
+    return daysRented;
+}
+
+
+public Movie getMovie() {
+    return movie;
+}
 }
